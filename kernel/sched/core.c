2db52d1837879ada0bcd6ec66e9862f9b8f55b81
Alexey Dobriyan <adobriyan@gmail.com>
2021-04-22 23:02:27 +0300

    sched: Make nr_iowait_cpu() return 32-bit value
    Runqueue ->nr_iowait counters are 32-bit anyway.
    
    Propagate 32-bitness into other code, but don't try too hard.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20210422200228.1423391-3-adobriyan@gmail.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4094,7 +4094,7 @@ unsigned int nr_iowait(void)
  * runnable.
  */
 
-unsigned long nr_iowait_cpu(int cpu)
+unsigned int nr_iowait_cpu(int cpu)
 {
 	struct rq *this = cpu_rq(cpu);
 	return atomic_read(&this->nr_iowait);
