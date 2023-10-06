4a77ed2b0bbb10fe469fe44216a90d07707d353c
Ingo Molnar <mingo@kernel.org>
2023-10-06 12:25:18 +0200

    sched/nohz: Remove unnecessarily complex error handling pattern from find_new_ilb()
    find_new_ilb() returns nr_cpu_ids on failure - which is the usual
    cpumask bitops return pattern, but is weird & unnecessary in this
    context: not only is it a global variable, it it is a +1 out of
    bounds CPU index and also has different signedness ...
    
    Its only user, kick_ilb(), then checks the return against nr_cpu_ids
    to decide to return. There's no other use.
    
    So instead of this, use a standard -1 return on failure to find an
    idle CPU, as the argument is signed already.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Link: https://lore.kernel.org/r/20231006102518.2452758-4-mingo@kernel.org
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12146,7 +12146,7 @@ static inline int find_new_ilb(void)
 			return ilb;
 	}
 
-	return nr_cpu_ids;
+	return -1;
 }
 
 /*
@@ -12165,8 +12165,7 @@ static void kick_ilb(unsigned int flags)
 		nohz.next_balance = jiffies+1;
 
 	ilb_cpu = find_new_ilb();
-
-	if (ilb_cpu >= nr_cpu_ids)
+	if (ilb_cpu < 0)
 		return;
 
  	/*
