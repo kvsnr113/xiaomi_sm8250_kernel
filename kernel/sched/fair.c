68dd5300e7d8ee43e0a4655313f8bf60d355ed8b
Ke Zhao <zhaoke09@baidu.com>
2025-05-13 14:18:45 +0800

    sched/fair: Remove unused function and related comment
    Different from min_vruntime(), there is no function actually
    using max_vruntime() now. So there is no reason to keep it,
    just remove this function and related comment.
    
    Signed-off-by: Ke Zhao <zhaoke09@baidu.com>
    Link: https://lore.kernel.org/lkml/20250513061845.13868-1-zhaoke09@baidu.com/
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -493,15 +493,6 @@ void account_cfs_rq_runtime(struct cfs_rq *cfs_rq, u64 delta_exec);
  * Scheduling class tree data structure manipulation methods:
  */
 
-static inline u64 max_vruntime(u64 max_vruntime, u64 vruntime)
-{
-	s64 delta = (s64)(vruntime - max_vruntime);
-	if (delta > 0)
-		max_vruntime = vruntime;
-
-	return max_vruntime;
-}
-
 static inline u64 min_vruntime(u64 min_vruntime, u64 vruntime)
 {
 	s64 delta = (s64)(vruntime - min_vruntime);
