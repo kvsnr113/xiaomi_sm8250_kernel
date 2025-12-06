828e78de8607f04017a7e8cc73a5d7e5a53fb7d4
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-12-06 21:01:53 +0700

    e404_attr: drop kernelsu runtime init blocker
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/misc/e404_attributes.c
+++ drivers/misc/e404_attributes.c
@@ -3,7 +3,6 @@
 #include <linux/e404_attributes.h>
 
 struct e404_attributes e404_data = {
-    .kernelsu = 0,
     .effcpu = 0,
     .rom_type = 1,
     .dtbo_type = 1,
@@ -17,12 +16,6 @@ struct e404_attributes e404_data = {
 };
 
 static struct kobject *e404_kobj;
-
-#ifdef CONFIG_E404_KSU_DEFAULT
-int early_kernelsu = 1;
-#else
-int early_kernelsu = 0;
-#endif
 
 #ifdef CONFIG_E404_EFFCPU_DEFAULT
 int early_effcpu = 1;
@@ -57,11 +50,7 @@ static int __init parse_e404_args(char *str)
 
         pr_alert("E404: Parsing flag: %s\n", arg);
 
-        if (strcmp(arg, "root_ksu") == 0)
-            early_kernelsu = 1;
-        else if (strcmp(arg, "root_noksu") == 0)
-            early_kernelsu = 0;
-        else if (strcmp(arg, "dtb_effcpu") == 0)
+        if (strcmp(arg, "dtb_effcpu") == 0)
             early_effcpu = 1;
         else if (strcmp(arg, "dtb_def") == 0)
             early_effcpu = 0;
@@ -88,14 +77,12 @@ static int __init parse_e404_args(char *str)
 early_param("e404_args", parse_e404_args);
 
 static void e404_parse_attributes(void) {
-    e404_data.kernelsu = early_kernelsu;
     e404_data.effcpu = early_effcpu;
     e404_data.rom_type = early_rom_type;
     e404_data.dtbo_type = early_dtbo_type;
     e404_data.batt_profile = early_batt_profile;
 
-    pr_alert("E404 Early Attributes: KernelSU=%d, EFFCPU=%d, RomType=%d, DTBOType=%d, BatteryProfile=%d\n",
-        e404_data.kernelsu,
+    pr_alert("E404 Early Attributes: EFFCPU=%d, RomType=%d, DTBOType=%d, BatteryProfile=%d\n",
         e404_data.effcpu,
         e404_data.rom_type,
         e404_data.dtbo_type,
@@ -124,7 +111,6 @@ static ssize_t name##_store(struct kobject *kobj, struct kobj_attribute *attr, c
 } \
 static struct kobj_attribute name##_attr = __ATTR(name, 0664, name##_show, name##_store);
 
-E404_ATTR_RO(kernelsu);
 E404_ATTR_RO(effcpu);
 E404_ATTR_RO(rom_type);
 E404_ATTR_RO(dtbo_type);
@@ -138,7 +124,6 @@ E404_ATTR_RW(kgsl_skip_zeroing);
 E404_ATTR_RW(file_sync);
 
 static struct attribute *e404_attrs[] = {
-    &kernelsu_attr.attr,
     &effcpu_attr.attr,
     &rom_type_attr.attr,
     &dtbo_type_attr.attr,
