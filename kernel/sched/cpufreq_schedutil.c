c2ac67498b44187def495a0dbbaa4edc27bd124c
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-02-21 23:21:22 +0700

    cpufreq_schedutil: simple flat +25% dvfs headroom boosting
    this is probably the best one i can say:
    - small cpu cluster doesn't sit on max freq during idle
    - no more random aggresive freq spikes, it actually chills now
    - ui doesn't seem to stutter at all, in fact it is smoother than quadratic
    - the computation is very very cheap compared to quadratic
    
    the only downsides is that uibench shows +0.2ms regression, but this is due to quadratic basically capping the highest freq possible.
    so syntethic benchmark is nothing to worry about.
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -230,27 +230,14 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
-	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta, headroom, min_util;
+    unsigned long cap = capacity_orig_of(cpu);
 
-	if (util >= capacity)
-		return util;
-        /*
-        * Quadratic taper the boosting at the top end as these are expensive
-        * and we don't need that much of a big headroom as we approach max
-        * capacity
-        */
-	delta = capacity - util;
-	headroom = ((delta * delta) >> 12);
+	if (util < (cap >> 4)) // suppress noise < ~6.25%
+        return util;
 
-	/* 10% of capacity threshold */
-	min_util = capacity / 10;
+    util += util >> 2; // flat 25% headroom
 
-	/* Suppress boosting below the threshold */
-	if (util < min_util)
-		headroom = (headroom * util * util) / (min_util * min_util);
-
-	return util + headroom;
+    return min(util, cap);
 }
 
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
